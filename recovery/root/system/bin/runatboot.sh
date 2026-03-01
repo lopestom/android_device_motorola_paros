98f51102df38f4113e3965458b2187fd401c14fd
lopestorm <guillopes2002@gmail.com>
2026-03-01 19:13:48 -0300

    Add files via upload
    device paros.

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -5,13 +5,13 @@ setenforce 0
 
 mount /vendor_dlkm
 mount /vendor
-modprobe -d /vendor_dlkm/lib/modules /vendor_dlkm/lib/modules/goodix_brl_mmi.ko
-modprobe -d /vendor_dlkm/lib/modules /vendor_dlkm/lib/modules/touchscreen_mmi.ko
+modprobe -d /vendor_dlkm/lib/modules /vendor_dlkm/lib/modules/focaltech_0flash_v2_mmi.ko
+modprobe -d /vendor_dlkm/lib/modules /vendor_dlkm/lib/modules/ili9882_mmi.ko
 sleep 1
-if [ $(cat /sys/class/touchscreen/primary/productinfo) == "gt9916S" ]
+if [ $(cat /sys/class/touchscreen/primary/productinfo) == "ft8725" ]
 then
 echo 1 > /sys/class/touchscreen/primary/forcereflash
-echo goodix-boe-gt9916S-24010501-6597AA91-avatrn.bin > /sys/class/touchscreen/primary/doreflash
+echo focaltech_ts_fw_.bin > /sys/class/touchscreen/primary/doreflash
 echo "Reflashing Firmware..."
 echo 0 > /sys/class/touchscreen/primary/forcereflash
 fi
@@ -19,16 +19,18 @@ fi
 is_fastboot=$(getprop init.svc.fastbootd)
 if [ "$is_fastboot" != "running" ]; then
     mount /vendor_dlkm
+        insmod /vendor_dlkm/lib/modules/ili9882_mmi.ko
+        insmod /vendor_dlkm/lib/modules/leds_aw99703.ko
         insmod /vendor_dlkm/lib/modules/msm_drm.ko
         insmod /vendor_dlkm/lib/modules/mmi_annotate.ko
         insmod /vendor_dlkm/lib/modules/mmi_info.ko
         insmod /vendor_dlkm/lib/modules/mmi_relay.ko
         insmod /vendor_dlkm/lib/modules/mmi_charger.ko
         insmod /vendor_dlkm/lib/modules/mmi_sys_temp.ko
+        insmod /vendor_dlkm/lib/modules/moto_f_mass_storage.ko
         insmod /vendor_dlkm/lib/modules/sensors_class.ko
         insmod /vendor_dlkm/lib/modules/touchscreen_mmi.ko
-        insmod /vendor_dlkm/lib/modules/stmicro_mmi.ko
-        insmod /vendor_dlkm/lib/modules/goodix_brl_mmi.ko
+        insmod /vendor_dlkm/lib/modules/goodix_fod_mmi.ko
         insmod /vendor_dlkm/lib/modules/sx937x_sar.ko
         insmod /vendor_dlkm/lib/modules/bm_adsp_ulog.ko
         insmod /vendor_dlkm/lib/modules/qti_glink_charger.ko
@@ -39,10 +41,9 @@ if [ "$is_fastboot" != "running" ]; then
         insmod /vendor_dlkm/lib/modules/gpr_dlkm.ko
         insmod /vendor_dlkm/lib/modules/spf_core_dlkm.ko
         insmod /vendor_dlkm/lib/modules/adsp_loader_dlkm.ko
-        insmod /vendor_dlkm/lib/modules/moto_f_mass_storage.ko
+        insmod /vendor_dlkm/lib/modules/focaltech_0flash_v2_mmi.ko
         insmod /vendor_dlkm/lib/modules/phy-msm-ssusb-qmp.ko
         insmod /vendor_dlkm/lib/modules/qdss_bridge.ko
-        insmod /vendor_dlkm/lib/modules/ssusb-redriver-ps5169.ko
         insmod /vendor_dlkm/lib/modules/usb_f_qdss.ko
         insmod /vendor_dlkm/lib/modules/moto_f_usbnet.ko
 fi
